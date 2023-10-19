f0d14c2fc29af33a2da495362bc837da8d9283e0
Amey Gurunath Parab <ameygurunathparab@gmail.com>
2023-10-19 18:05:17 -0400

    Fixed modules component

--- src/Kanbas/Courses/CourseHeader/index.js
+++ src/Kanbas/Courses/CourseHeader/index.js
@@ -5,19 +5,21 @@ import './index.css';
 function CourseHeader() {
   return (
     <div className="wd-modules-screen mt-3 ms-auto">
-      <button className="btn wd-bg-lightgray  me-1">Collapse All</button>
-      <button class="btn wd-bg-lightgray me-1">View Progress</button>
-      <select class="btn wd-bg-lightgray me-1">
-        <option selected>Publish All</option>
-      </select>
-      <button class="btn btn-danger me-1">
-        <AiOutlinePlus />
-        Module
-      </button>
-      <button class="btn wd-bg-lightgray">
-        <HiOutlineEllipsisVertical />
-      </button>
-      <hr />
+      <div className="course-header">
+        <button className="btn wd-bg-lightgray  me-1">Collapse All</button>
+          <button class="btn wd-bg-lightgray me-1">View Progress</button>
+          <select class="btn wd-bg-lightgray me-1">
+            <option selected>Publish All</option>
+          </select>
+          <button class="btn btn-danger me-1">
+            <AiOutlinePlus />
+            Module
+          </button>
+          <button class="btn wd-bg-lightgray">
+            <HiOutlineEllipsisVertical />
+          </button>
+      </div>
+      <hr/>
     </div>
   );
 }
